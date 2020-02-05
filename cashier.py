9ec4c538add8967fb3e5a6e048d0f10ac99c0857
root <root@localhost.localdomain>
2020-02-05 05:48:14 -0500

    adding a cashier

--- cashier.py
+++ cashier.py
@@ -1,5 +1,35 @@
 def main():
-	# write code here
+        # write code her
+    items = []
+    done=True
+    while done:
+        name = input('Item (enter "done" when finished): ')
+        if name.lower() == "done":
+                print()
+                print("-"*20)
+                print("receipt")
+                print("-"*20)
+                for i in items:
+                    print(str(i["quantity"]) + " " +i["name"]+" " +str(i["price"]*i["quantity"]))
+                done = False
+                
+
+        else:
+          price = float(input("Price: "))
+          quantity = int(input("Quantity: "))
+          dicts={"name":name,"price":price, "quantity":quantity}
+          items.append(dicts)
+        
+      #  if name.lower() == "done":
+      #      print()
+      #      print("-"*20)
+      #      print("receipt")
+      #      print("-"*20)
+      #      for i in item:
+      #          print(str(i["quantity"]) + " " +i["name"]+" " +str(i["price"]*i["quantity"])
+
+
 
 if __name__ == '__main__':
-	main()
+     main()
+
